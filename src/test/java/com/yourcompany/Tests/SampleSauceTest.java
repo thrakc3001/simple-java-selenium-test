a45935b9efa3144b287c27d29bfed5f8c6840ede
Matt Cain <30560401+thrakc3001@users.noreply.github.com>
2020-04-23 22:09:00 -0500

    Update SampleSauceTest.java

--- src/test/java/com/yourcompany/Tests/SampleSauceTest.java
+++ src/test/java/com/yourcompany/Tests/SampleSauceTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
