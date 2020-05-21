00e4f4c96655b02384253c00f95872dfb847911d
Matt Cain <30560401+thrakc3001@users.noreply.github.com>
2020-05-21 14:00:22 -0500

    Update SampleSauceTest.java

--- src/test/java/com/yourcompany/Tests/SampleSauceTest.java
+++ src/test/java/com/yourcompany/Tests/SampleSauceTest.java
@@ -47,7 +47,7 @@ public class SampleSauceTest {
 	capabilities.setCapability("name", "Jenkins Test");
 	capabilities.setCapability("deviceName", System.getenv("SELENIUM_DEVICE"));
     	capabilities.setCapability("platformName", System.getenv("SELENIUM_DEVICE_TYPE"));
-// 	capabilities.setCapability("build", System.getenv("JOB_NAME") + "__" + System.getenv("BUILD_NUMBER"));
+	capabilities.setCapability("build", System.getenv("JOB_NAME") + "__" + System.getenv("BUILD_NUMBER"));
 
 
     driver = new RemoteWebDriver(new URL(URL), capabilities);
@@ -68,9 +68,9 @@ public class SampleSauceTest {
     // SauceREST sauce = new SauceREST(System.getenv("SAUCE_USERNAME"), System.getenv("SAUCE_ACCESS_KEY"));
     // sauce.jobPassed(sessionId);
 	  
-//     String message = String.format("SauceOnDemandSessionID=%1$s job-name=%2$s",
-//     (((RemoteWebDriver) driver).getSessionId()).toString(), "Jenkins Test");
-//     System.out.println(message);
+    String message = String.format("SauceOnDemandSessionID=%1$s job-name=%2$s",
+    (((RemoteWebDriver) driver).getSessionId()).toString(), System.getenv("JOB_NAME"));
+    System.out.println(message);
 	  
 
     driver.quit();
